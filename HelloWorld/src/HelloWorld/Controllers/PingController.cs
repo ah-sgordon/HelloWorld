5e119eef70b16e89ef52368fb443789e32a27b43
Steve Gordon <steve.gordon@physiciansinteractive.com>
2016-02-19 17:18:36 -0500

    - Added Dockerfile - Added controller for a ping check - Added a list of integers to the Record model to demonstrate lists functionality in DynamoDB

---
+++ HelloWorld/src/HelloWorld/Controllers/PingController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNet.Mvc;
+
+namespace HelloWorld.Controllers
+{
+    [Route("[controller]")]
+    public class PingController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Success!");
+        }
+    }
+}
--- HelloWorld/src/HelloWorld/Models/Record.cs
+++ HelloWorld/src/HelloWorld/Models/Record.cs
@@ -1,8 +1,12 @@
-﻿namespace HelloWorld.Models
+﻿using System.Collections.Generic;
+
+namespace HelloWorld.Models
 {
     public class Record
     {
         public string Id { get; set; }
         public string Value { get; set; }
+
+        public List<string> CategoryIdList { get; set; }
     }
 }
